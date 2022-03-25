b2721cf8f018b45700ccd06d76775f2aec95c383
Richard Darst <rkd@zgib.net>
2022-03-25 16:51:58 +0200

    Remove now-unneded local style

--- content/conf.py
+++ content/conf.py
@@ -72,7 +72,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['css']
+#html_static_path = ['css']
 
 
 # HTML context:
@@ -102,6 +102,3 @@ html_context = {
 #    #'matplotlib': ('https://matplotlib.org/', None),
 #    'seaborn': ('https://seaborn.pydata.org/', None),
 # }
-
-def setup(app):
-    app.add_css_file("style.css")
